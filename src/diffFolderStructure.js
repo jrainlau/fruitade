f58b2a1dfaedce7a80fa0eabd9a80b74720eafb4
jrainliu <jrainliu@tencent.com>
2023-08-19 10:33:06 +0800

    fix: support empty folders

--- src/diffFolderStructure.js
+++ src/diffFolderStructure.js
@@ -9,15 +9,18 @@ function diffFolderStructure(a, b) {
   function traverse(a, b, path = []) {
     for (const key in a) {
       if (!(key in b)) {
-        result.deleted[path.concat(key).join("/")] = a[key];
+        result.deleted[path.concat(key).join('/')] = a[key];
       } else {
-        if (typeof a[key] === "object" && typeof b[key] === "object") {
+        if (typeof a[key] === 'object' && typeof b[key] === 'object') {
+          if (isEmptyObject(a[key]) && isEmptyObject(b[key])) {
+            result.unchanged[path.concat(key).join('/')] = ''
+          }
           traverse(a[key], b[key], path.concat(key));
         } else {
           if (a.size !== b.size || a.md5 !== b.md5) {
-            result.modified[path.join("/")] = b;
+            result.modified[path.join('/')] = b;
           } else {
-            result.unchanged[path.join("/")] = a;
+            result.unchanged[path.join('/')] = a;
           }
         }
       }
@@ -25,27 +28,31 @@ function diffFolderStructure(a, b) {
 
     for (const key in b) {
       if (!(key in a)) {
-        result.added[path.concat(key).join("/")] = b[key];
+        result.added[path.concat(key).join('/')] = b[key] || '';
       }
     }
   }
 
   traverse(a, b);
 
-  function flattenObject(obj, prefix = '', result = {}) {
-    for (let key in obj) {
-      if (!obj[key].filename) {
-        flattenObject(obj[key], `${prefix}${key}/`, result);
-      } else {
-        result[`${prefix}${key}`] = obj[key];
-      }
-    }
-    return result;
-  }
-
   result.added = flattenObject(result.added)
 
   return result;
+}
+
+function flattenObject(obj, prefix = '', result = {}) {
+  for (let key in obj) {
+    if (!obj[key].filename) {
+      flattenObject(obj[key], `${prefix}${key}/`, result);
+    } else {
+      result[`${prefix}${key}`] = obj[key];
+    }
+  }
+  return result;
+}
+
+function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0
 }
 
 module.exports = diffFolderStructure
